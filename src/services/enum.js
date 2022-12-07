43b7142d298d9655e4ae15c9cb94dcd6684569d7
ngotanthanh20-10 <thanhngoangiang@gmail.com>
2022-12-07 21:03:57 +0700

    init

---
+++ src/services/enum.js
@@ -0,0 +1,11 @@
+export const API_KEY = {
+    API_KEY_MOVIE: "afefe489265de1ed96ac6906c7332807",
+    API_KEY_FIREBASE: {
+        API_KEY: "AIzaSyBGYeTrPG4SsWCKO7N4U7A5c3suS5WYJ8w",
+        AUTH_DOMAIN: "netflix-clone-f18c1.firebaseapp.com",
+        PROJECT_ID: "netflix-clone-f18c1",
+        STORAGE_BUCKET: "netflix-clone-f18c1.appspot.com",
+        MESSAGING_SENDER_ID: "275660285477",
+        APP_ID: "1:275660285477:web:07770120458e61336b81cc"
+    }
+}
